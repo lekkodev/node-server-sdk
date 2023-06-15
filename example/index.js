8896d93e10e3ff9239ee2e6aa783481493ed1c6e
danlekko <128412491+danlekko@users.noreply.github.com>
2023-06-15 14:03:29 -0700

    Remove bin

--- example/index.js
+++ example/index.js
@@ -4,14 +4,14 @@ async function getFeature() {
   // Initialize the client, replace "repo_owner", "repo_name" and "lekko_api_key"
   const client = await lekko.initAPIClient(
     {
-      apiKey: "lekko_e43fc617-7788-4f1f-9c20-cf7302d8bd5a_0c58fed2-f88e-4991-9bc8-ad38b1d267b7",
-      repositoryOwner: "lekkodev",
-      repositoryName: "dan-prod-test"
+      apiKey: "lekko_apikey",
+      repositoryOwner: "repo_owner",
+      repositoryName: "repo_name"
     }
   )
   
   // Get a feature, replace "my_namespace", and "my_feature"
-  return await client.getBoolFeature("default", "example", new lekko.ClientContext().setString("asdf", "my_context_value"));
+  return await client.getBoolFeature("default", "example", new lekko.ClientContext().setString("my_context_key", "my_context_value"));
 }
 
 getFeature().then(feature => console.log(feature))
