d9dd97b8417e90b8f25a82c9824ef8a5a889b510
Shubhit Singh <3323832+shubhitms@users.noreply.github.com>
2023-06-21 17:23:06 -0700

    Use custom any (#7)

--- example/index.js
+++ example/index.js
@@ -8,10 +8,10 @@ async function getFeature() {
       repositoryOwner: "repo_owner",
       repositoryName: "repo_name"
     }
-  )
+  );
   
   // Get a feature, replace "my_namespace", and "my_feature"
   return await client.getBoolFeature("default", "example", new lekko.ClientContext().setString("my_context_key", "my_context_value"));
 }
 
-getFeature().then(feature => console.log(feature))
+getFeature().then(feature => console.log(feature));
--- src/__tests__/client.test.ts
+++ src/__tests__/client.test.ts
@@ -1,17 +1,21 @@
-import { ClientContext } from '../context';
-import { initAPIClient, initSidecarClient } from '../index';
 import {
   GetBoolValueRequest,
   GetBoolValueResponse,
-  GetIntValueRequest,
-  GetIntValueResponse,
   GetFloatValueRequest,
   GetFloatValueResponse,
+  GetIntValueRequest,
+  GetIntValueResponse,
   GetJSONValueRequest,
   GetJSONValueResponse,
+  GetProtoValueRequest,
+  GetProtoValueResponse,
   GetStringValueRequest,
   GetStringValueResponse,
+  Any as LekkoAny,
 } from '@buf/lekkodev_sdk.bufbuild_es/lekko/client/v1beta1/configuration_service_pb';
+import { Any, BoolValue } from '@bufbuild/protobuf';
+import { ClientContext } from '../context';
+import { initAPIClient, initSidecarClient } from '../index';
 
 test('build API client', async () => {
   const client = await initAPIClient({
@@ -50,11 +54,17 @@ test('get bool feature', async () => {
     repositoryOwner: "lekkodev",
     repositoryName: "config-test"
   });
-  client.client.getBoolValue = jest.fn().mockReturnValue(GetBoolValueResponse.fromJson({
+  const mockFn = jest.fn();
+  Object.defineProperty(client.client, "getBoolValue", {
+    value: mockFn,
+    configurable: true,
+    writable: true
+  });
+  jest.spyOn(client.client, "getBoolValue").mockImplementation(async () => GetBoolValueResponse.fromJson({
     value: true,
   }));
   expect(await client.getBoolFeature('types', 'bool', SAMPLE_CONTEXT)).toBe(true);
-  expect(client.client.getBoolValue.mock.lastCall[0]).toEqual(GetBoolValueRequest.fromJson({
+  expect(mockFn.mock.lastCall[0]).toEqual(GetBoolValueRequest.fromJson({
     key: 'bool',
     context: {
       sample_key: {
@@ -75,11 +85,17 @@ test('get int feature', async () => {
     repositoryOwner: "lekkodev",
     repositoryName: "config-test"
   });
-  client.client.getIntValue = jest.fn().mockReturnValue(GetIntValueResponse.fromJson({
+  const mockFn = jest.fn();
+  Object.defineProperty(client.client, "getIntValue", {
+    value: mockFn,
+    configurable: true,
+    writable: true
+  });
+  jest.spyOn(client.client, "getIntValue").mockImplementation(async () => GetIntValueResponse.fromJson({
     value: 42,
   }));
-  expect(await client.getIntFeature('types', 'int', SAMPLE_CONTEXT)).toEqual(BigInt(42));
-  expect(client.client.getIntValue.mock.lastCall[0]).toEqual(GetIntValueRequest.fromJson({
+  expect(await client.getIntFeature('types', 'int', SAMPLE_CONTEXT)).toEqual(42);
+  expect(mockFn.mock.lastCall[0]).toEqual(GetIntValueRequest.fromJson({
     key: 'int',
     context: {
       sample_key: {
@@ -100,11 +116,17 @@ test('get float feature', async () => {
     repositoryOwner: "lekkodev",
     repositoryName: "config-test"
   });
-  client.client.getFloatValue = jest.fn().mockReturnValue(GetFloatValueResponse.fromJson({
+  const mockFn = jest.fn();
+  Object.defineProperty(client.client, "getFloatValue", {
+    value: mockFn,
+    configurable: true,
+    writable: true
+  });
+  jest.spyOn(client.client, "getFloatValue").mockImplementation(async () => GetFloatValueResponse.fromJson({
     value: 3.14,
   }));
   expect(await client.getFloatFeature('types', 'float', SAMPLE_CONTEXT)).toBeCloseTo(3.14);
-  expect(client.client.getFloatValue.mock.lastCall[0]).toEqual(GetFloatValueRequest.fromJson({
+  expect(mockFn.mock.lastCall[0]).toEqual(GetFloatValueRequest.fromJson({
     key: 'float',
     context: {
       sample_key: {
@@ -131,11 +153,17 @@ test('get json feature', async () => {
       c: 'foobar',
     }
   };
-  client.client.getJSONValue = jest.fn().mockReturnValue(GetJSONValueResponse.fromJson({
+  const mockFn = jest.fn();
+  Object.defineProperty(client.client, "getJSONValue", {
+    value: mockFn,
+    configurable: true,
+    writable: true
+  });
+  jest.spyOn(client.client, "getJSONValue").mockImplementation(async () => GetJSONValueResponse.fromJson({
     value: Buffer.from(JSON.stringify(mockedValue)).toString('base64'),
   }));
   expect(await client.getJSONFeature('types', 'json', SAMPLE_CONTEXT)).toEqual(mockedValue);
-  expect(client.client.getJSONValue.mock.lastCall[0]).toEqual(GetJSONValueRequest.fromJson({
+  expect(mockFn.mock.lastCall[0]).toEqual(GetJSONValueRequest.fromJson({
     key: 'json',
     context: {
       sample_key: {
@@ -156,11 +184,17 @@ test('get string feature', async () => {
     repositoryOwner: "lekkodev",
     repositoryName: "config-test"
   });
-  client.client.getStringValue = jest.fn().mockReturnValue(GetStringValueResponse.fromJson({
+  const mockFn = jest.fn();
+  Object.defineProperty(client.client, "getStringValue", {
+    value: mockFn,
+    configurable: true,
+    writable: true
+  });
+  jest.spyOn(client.client, "getStringValue").mockImplementation(async () => GetStringValueResponse.fromJson({
     value: 'foobar',
   }));
   expect(await client.getStringFeature('types', 'string', SAMPLE_CONTEXT)).toBe('foobar');
-  expect(client.client.getStringValue.mock.lastCall[0]).toEqual(GetStringValueRequest.fromJson({
+  expect(mockFn.mock.lastCall[0]).toEqual(GetStringValueRequest.fromJson({
     key: 'string',
     context: {
       sample_key: {
@@ -172,6 +206,60 @@ test('get string feature', async () => {
       ownerName: 'lekkodev',
       repoName: 'config-test',
     },
+  }));
+});
+
+test('get proto feature', async () => {
+  const a = Any.pack(new BoolValue({
+    value: true
+  }));
+  const testWithResponse = async (resp: GetProtoValueResponse) => {
+    const client = await initAPIClient({
+      apiKey: "apiKey",
+      repositoryOwner: "lekkodev",
+      repositoryName: "config-test"
+    });
+    const mockFn = jest.fn();
+    Object.defineProperty(client.client, "getProtoValue", {
+      value: mockFn,
+      configurable: true,
+      writable: true
+    });
+    jest.spyOn(client.client, "getProtoValue").mockImplementation(async () => resp);
+    expect(await client.getProtoFeature('types', 'proto', SAMPLE_CONTEXT)).toEqual(a);
+    expect(mockFn.mock.lastCall[0]).toEqual(GetProtoValueRequest.fromJson({
+      key: 'proto',
+      context: {
+        sample_key: {
+          stringValue: 'sample_value',
+        },
+      },
+      namespace: 'types',
+      repoKey: {
+        ownerName: 'lekkodev',
+        repoName: 'config-test',
+      },
+    }));
+  };
+
+  // test backwards compatibility
+  await testWithResponse(new GetProtoValueResponse({
+    value: a,
+  }));
+
+  // test dual-writing old and new any
+  await testWithResponse(new GetProtoValueResponse({
+    value: a,
+    valueV2: new LekkoAny({
+      ...a
+    })
+  }));
+  
+  // test new any only
+  await testWithResponse(new GetProtoValueResponse({
+    valueV2: new LekkoAny({
+      ...a
+    })
   }));
 });
 
