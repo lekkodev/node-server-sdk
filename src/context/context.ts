891f7c31f789a9505941c2084f715fa6bb931473
Sergey Passichenko <sergey@lekko.com>
2024-02-14 15:58:19 -0800

    create context from json (#26)
    * create context from json
    
    * restict json context
    
    * fromJSON

--- src/context/context.ts
+++ src/context/context.ts
@@ -1,4 +1,4 @@
-import { Value } from '@buf/lekkodev_sdk.bufbuild_es/lekko/client/v1beta1/configuration_service_pb';
+import { Value } from "@buf/lekkodev_sdk.bufbuild_es/lekko/client/v1beta1/configuration_service_pb";
 
 type ContextKey = string;
 
@@ -41,12 +41,40 @@ class ClientContext {
     return this;
   }
 
+  static fromJSON(jsonContext?: {
+    [key: string]: string | number | boolean;
+  }): ClientContext {
+    const ctx = new ClientContext();
+    if (jsonContext === undefined) {
+      return ctx;
+    }
+    Object.entries(jsonContext).forEach(([k, v]) => {
+      switch (typeof v) {
+        case "number":
+          // TODO: `1.0` is still integer in js :(
+          if (Number.isInteger(v)) {
+            ctx.setInt(k, v);
+          } else {
+            ctx.setDouble(k, v);
+          }
+          break;
+        case "string":
+          ctx.setString(k, v);
+          break;
+        case "boolean":
+          ctx.setBoolean(k, v);
+          break;
+      }
+    });
+    return ctx;
+  }
+
   toString(): string {
     const pairs: string[] = [];
     for (const k in this.data) {
       pairs.push(`${k}: ${this.data[k].kind.value}`);
     }
-    return pairs.join(', ');
+    return pairs.join(", ");
   }
 }
 
