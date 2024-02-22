36dc5c0916a2c9cd94e79012444bbde3773e6263
Jonathan Dobbie <jonathan@lekko.com>
2024-02-21 16:11:14 -0800

    more todos

--- src/ts-to-lekko.ts
+++ src/ts-to-lekko.ts
@@ -12,6 +12,11 @@ import { TypeChecker } from "typescript";
     * Correctly parse the binaryExpressions to validate property and convert to the correct operators
     * rewrite the code to be /less/ vomit inducing
     * wrapper meta-programming code to use it as a static fallback
+    
+    Stretch:
+    * Static Contexts
+    * Code gen from the repo (should be easy just not needed now)
+    * config description as jsdoc
 
 */
 
